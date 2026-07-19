72ceca52ec0c45244b7a5f6d79c580f3d4011569
Phantomita <tomtom.dev29@gmail.com>
2026-07-19 14:59:27 +0300

    initial project generation

---
+++ src/main/java/com/sporty/jackpot_service/JackpotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sporty.jackpot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackpotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackpotServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sporty/jackpot_service/JackpotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sporty.jackpot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JackpotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
